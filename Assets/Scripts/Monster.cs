b443411a5ad793a5c38b2f055c7a4866ebc6f041
Sebimmortal <Seb.immortal@gmail.com>
2023-10-26 12:45:06 -0600

    added navmesh

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
     public Animator anim;
-    
+    public NavMeshAgent agent;
+
+    public Vector3 playerPos = GetComponent<Player>.transform.Position;
+
     void Start()
     {
         transform.Rotate(0, 90, 0);
+    }
+
+    void FixedUpdate()
+    {
+        agent.SetDestination(Player.Transform.Position);
     }
 
     void Sprint()
